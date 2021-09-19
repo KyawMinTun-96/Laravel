efd6bbd18b94d3edba4b1a12f473fb63983ea621
mgkyawmin <kyawmintun.kmt4142020@gmail.com>
2021-09-19 12:10:32 +0630

    project setup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.master')
+
+@section('title', 'Home Page')
+
+@section('content')
+    <h1>This is the home page.</h1>
+    @endsection
---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+
+    <!-- fontawsome -->
+    <link rel="stylesheet" href="{{asset('assets/css/all.min.css')}}">
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+
+    <!-- style css -->
+    <link rel="stylesheet" href="{{asset('assets/css/reset.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+</head>
+<body>
+
+
+    @yield('content')
+
+
+
+    <!-- jQuery -->
+    <script src="{{asset('assets/js/jquery-3.6.0.js')}}"></script>
+    <script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('assets/js/app.js')}}"></script>
+    
+</body>
+</html>
